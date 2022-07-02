2a522dd371ec4d1adb198b486195585542a4356d
SuperVlodya <98913672+SuperVlodya@users.noreply.github.com>
2022-07-02 19:17:31 +0300

    Add files via upload

---
+++ Extra_HA_4_1_config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import yaml
+
+
+class Config:
+    def __init__(self):
+        with open('config.yaml', 'r') as f:
+            self._config = yaml.load(f, Loader=yaml.FullLoader)
+        # print(self._config['requests'][0]['method'])
+
+    @property
+    def config(self):
+        return self._config
+
+    def __call__(self):
+        return self._config
+
+    @property
+    def requests(self) -> list:
+        return self._config['requests']
+
+    @property
+    def stages(self) -> list:
+        return self._config['STAGES']
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
